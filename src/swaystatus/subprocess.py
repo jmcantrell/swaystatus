b9335e9044372ecbd951124619bd945fa76c8f6b
Jeremy Cantrell <jmcantrell@gmail.com>
2024-01-26 15:24:00 -0600

    This does not need to be a static method

--- src/swaystatus/subprocess.py
+++ src/swaystatus/subprocess.py
@@ -2,21 +2,21 @@ from threading import Thread
 from subprocess import Popen, PIPE
 
 
+def proxy_lines(pipe, handler):
+    with pipe:
+        for line in pipe:
+            handler(line)
+
+
 class PopenStreamHandler(Popen):
     """
     Just like `Popen`, but handle stdout and stderr output in dedicated
     threads.
     """
 
-    @staticmethod
-    def _proxy_lines(pipe, handler):
-        with pipe:
-            for line in pipe:
-                handler(line)
-
     def __init__(self, stdout_handler, stderr_handler, *args, **kwargs):
         kwargs["stdout"] = PIPE
         kwargs["stderr"] = PIPE
         super().__init__(*args, **kwargs)
-        Thread(target=self._proxy_lines, args=[self.stdout, stdout_handler]).start()
-        Thread(target=self._proxy_lines, args=[self.stderr, stderr_handler]).start()
+        Thread(target=proxy_lines, args=[self.stdout, stdout_handler]).start()
+        Thread(target=proxy_lines, args=[self.stderr, stderr_handler]).start()
