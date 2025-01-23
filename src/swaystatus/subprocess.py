d3ef09c882fdb5d65e7877645872f6b7c2098c68
Jeremy Cantrell <jmcantrell@gmail.com>
2025-01-23 12:04:04 -0500

    Remove helper function from module namespace

--- src/swaystatus/subprocess.py
+++ src/swaystatus/subprocess.py
@@ -2,12 +2,6 @@ from subprocess import Popen, PIPE
 from threading import Thread
 
 
-def proxy_context(context, handler):
-    with context as lines:
-        for line in lines:
-            handler(line)
-
-
 class PopenStreamHandler(Popen):
     """
     Just like `Popen`, but handle stdout and stderr output in dedicated
@@ -15,8 +9,15 @@ class PopenStreamHandler(Popen):
     """
 
     def __init__(self, stdout_handler, stderr_handler, *args, **kwargs):
+        def proxy(context, handler):
+            with context as lines:
+                for line in lines:
+                    handler(line)
+
         kwargs["stdout"] = PIPE
         kwargs["stderr"] = PIPE
+
         super().__init__(*args, **kwargs)
-        Thread(target=proxy_context, args=[self.stdout, stdout_handler]).start()
-        Thread(target=proxy_context, args=[self.stderr, stderr_handler]).start()
+
+        Thread(target=proxy, args=[self.stdout, stdout_handler]).start()
+        Thread(target=proxy, args=[self.stderr, stderr_handler]).start()
