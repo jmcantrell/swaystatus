3eb7cad031ed55a0e1da44a6dcfee9fa55ae1bb1
Jeremy Cantrell <jmcantrell@gmail.com>
2025-11-04 22:28:42 -0600

    Add debugging for output

--- src/swaystatus/threading.py
+++ src/swaystatus/threading.py
@@ -30,6 +30,7 @@ class OutputWriter:
         logger.info("starting output")
         self._running.set()
         for blocks in self.output_generator.process(self.file):
+            logger.debug(f"processed output: {blocks}")
             self._tick.clear()
             self._tick.wait(self.interval)
             if not self._running.is_set():
