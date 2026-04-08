11db1bfd52b59ecbc94a358338c033858f883136
Jeremy Cantrell <jmcantrell@gmail.com>
2026-04-08 17:52:08 -0400

    Also accept path-like objects

--- src/swaystatus/paths.py
+++ src/swaystatus/paths.py
@@ -1,5 +1,6 @@
+import os
 from pathlib import Path
 
 
-def path_normalized(value: str) -> Path:
+def path_normalized(value: os.PathLike[str] | str) -> Path:
     return Path(value).expanduser().resolve()
--- tests/test_paths.py
+++ tests/test_paths.py
@@ -4,18 +4,30 @@ from unittest import TestCase, main
 from swaystatus.paths import path_normalized
 
 
+class PathLike:
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    def __fspath__(self) -> str:
+        return self.path
+
+
 class TestPathNormalized(TestCase):
     def test_absolute(self) -> None:
         self.assertEqual(path_normalized("/path"), Path("/path"))
+        self.assertEqual(path_normalized(PathLike("/path")), Path("/path"))
 
     def test_relative(self) -> None:
         self.assertEqual(path_normalized("path"), Path.cwd() / "path")
+        self.assertEqual(path_normalized(PathLike("path")), Path.cwd() / "path")
 
     def test_user(self) -> None:
         self.assertEqual(path_normalized("~/path"), Path.home() / "path")
+        self.assertEqual(path_normalized(PathLike("~/path")), Path.home() / "path")
 
     def test_resolve(self) -> None:
         self.assertEqual(path_normalized("/dir/../path"), Path("/path"))
+        self.assertEqual(path_normalized(PathLike("/dir/../path")), Path("/path"))
 
 
 if __name__ == "__main__":
