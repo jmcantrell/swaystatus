a68e96aa56af71910843280aff836ea9ce4d996a
Jeremy Cantrell <jmcantrell@gmail.com>
2026-08-24 23:54:23 -0500

    test(cli): demote unnecessary instance attribute
    Attaching the fake exception to the test instance is unnecessary as it's
    only used in a single test.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,27 +12,27 @@ class TestMain(TestCase):
         self.app_mock = app_patcher.start()
         self.addCleanup(app_patcher.stop)
 
-        self.fake_exc = Exception("BOOM!")
-
     def test_exit(self) -> None:
         self.assertEqual(cli.main(), 0, "expected a zero status")
         self.app_mock.return_value.run.assert_called_once()
 
     def test_raises(self) -> None:
+        fake_exc = Exception("BOOM!")
+
         for source, mock in [
             ("init", self.app_mock),
             ("start", self.app_mock.return_value.start),
         ]:
             with self.subTest(source=source):
                 self.app_mock.reset_mock()
-                mock.side_effect = self.fake_exc
+                mock.side_effect = fake_exc
 
                 with self.assertLogs(logger, logging.ERROR) as logged:
                     self.assertEqual(cli.main(), 1, "expected a non-zero status")
 
                 record = logged.records[0]
                 assert record.exc_info
-                self.assertIs(record.exc_info[1], self.fake_exc)
+                self.assertIs(record.exc_info[1], fake_exc)
                 self.assertEqual(record.message, "unhandled exception in app")
 
 
